77d667f49853963cdd07150f3d201a2739d2046f
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-08-08 20:25:50 -0700

    updated server file with require routes and handlebars

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars")
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+
+var PORT = 3000;
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
